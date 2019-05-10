cba1881475b0eb4db943648572f5c5af79545d8c
Robin Long <270361686@qq.com>
2019-05-10 10:32:46 +0800

    update text

--- src/main/java/guru/springfamework/controllers/v1/VendorController.java
+++ src/main/java/guru/springfamework/controllers/v1/VendorController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 /**
  * Created by jt on 10/6/17.
  */
-@Api(description = "This is my Vendor API")
+@Api(description = "This is my Vendor API from Robin")
 @RestController
 @RequestMapping(VendorController.BASE_URL)
 public class VendorController {
